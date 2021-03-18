1ce05043bc7acc36062b1f4de7100f20ef02bf08
chillichillibang <79812978+chillichillibang@users.noreply.github.com>
2021-03-18 23:51:48 +1100

    Initial commit of SelectionTask3

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SelectionsTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Username;
+            Console.Write("Please enter your Username: ");
+            Username = Console.ReadLine();
+
+            if (Username == "Jarrod")
+            {
+                Console.Write("Please enter your password: ");
+            }
+
+            else 
+            {
+                Console.Write("Login unsuccessful! Sorry, that Username does not exist.");
+                return;
+            }
+
+            string Password;
+            Password = Console.ReadLine();
+
+            if (Password == "admin")
+            {
+                Console.Write("Login successful!");
+            }
+
+            else
+            {
+                Console.Write("Login unsuccessful! Incorrect Password.");
+            }
+        }
+    }
+}
